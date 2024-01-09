d0e5183ef315d9d12f23fbdb70af733f1a1f884f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-09 07:45:59 -0300

    feat: implement get post author

---
+++ src/main/java/com/github/riannegreiros/springboard/dto/UserDto.java
@@ -0,0 +1,6 @@
+package com.github.riannegreiros.springboard.dto;
+
+import java.util.UUID;
+
+public record UserDto(UUID id, String name) {
+}
--- src/main/java/com/github/riannegreiros/springboard/resolvers/PostResolver.java
+++ src/main/java/com/github/riannegreiros/springboard/resolvers/PostResolver.java
@@ -1,12 +1,17 @@
 package com.github.riannegreiros.springboard.resolvers;
 
 import com.github.riannegreiros.springboard.dto.PostDto;
+import com.github.riannegreiros.springboard.dto.UserDto;
+import com.github.riannegreiros.springboard.entities.PostEntity;
 import com.github.riannegreiros.springboard.services.PostService;
+import com.github.riannegreiros.springboard.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostResolver {
@@ -14,8 +19,21 @@ public class PostResolver {
     @Autowired
     PostService postService;
 
+    @Autowired
+    UserService userService;
+
     @QueryMapping
     public List<PostDto> getPosts() {
         return postService.getPosts();
+    }
+
+    @SchemaMapping(typeName = "Post")
+    public Optional<UserDto> author(PostEntity post) {
+        var postId = post.getId();
+        if (postId == null) {
+            throw new RuntimeException("Post id can no be null");
+        }
+
+        return userService.findByPostId(postId);
     }
 }
