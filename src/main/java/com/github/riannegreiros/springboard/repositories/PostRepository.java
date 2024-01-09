e3059e7bf0aa3cbccd2fcf8d68057ac7469ebda4
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-01-09 07:02:43 -0300

    chore: setup core

---
+++ src/main/java/com/github/riannegreiros/springboard/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.github.riannegreiros.springboard.repositories;
+
+import com.github.riannegreiros.springboard.entities.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, UUID> {
+}
